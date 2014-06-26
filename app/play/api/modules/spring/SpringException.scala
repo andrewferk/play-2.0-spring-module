9e2eb9bc129f0f456211b15327f87315f5805fde
Deshayes Yann <deshayes.yann@gmail.com>
2014-06-26 15:16:55 +0200

    Migration play 2.3.1

--- app/play/api/modules/spring/SpringException.scala
+++ app/play/api/modules/spring/SpringException.scala
@@ -1,6 +1,6 @@
 package play.api.modules.spring
 
-import play.api.PlayException
+import play.api.UnexpectedException
 
 /**
  *
@@ -8,8 +8,10 @@ import play.api.PlayException
  * @since 12/10/11
  */
 
-class SpringException(title: String = "Spring context is not started!",
-                      description: String = "The Spring application context is not started.",
-                      cause: Option[Throwable] = None) extends PlayException(title, description, cause) {
+//class SpringException(title: String = "Spring context is not started!",
+//                     description: String = "The Spring application context is not started.",
+//                      cause: Option[Throwable] = None) extends PlayException(title, description, cause) {
 
+class SpringException( description: Option[String] = Option("The Spring application context is not started."),
+                      cause: Option[Throwable] = None) extends UnexpectedException(description, cause) {
 }
